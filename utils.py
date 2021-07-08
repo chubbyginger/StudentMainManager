6d39ccc6b4bfae8669d7e3675a5de58526fa1244
yangshunhuai <67225026+yangshunhuai@users.noreply.github.com>
2021-07-08 13:30:51 +0800

    Util: Update autoupdate util

--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import configparser
+
+def checkNTSD():
+    return os.path.isfile('./binaries/ntsd.exe')
+
+def checkPsSuspend():
+    return os.path.isfile('./binaries/pssuspend.exe')
+
+def checkJiYuTrainer():
+    return os.path.isfile('./binaries/JiYuTrainer/JiYuTrainer.exe')
+
+def checkUpdater():
+    return os.path.isfile('./AutoUpdate.exe')
